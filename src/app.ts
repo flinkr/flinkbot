75291f962dd0ea659e04862a1d971303e46e7dce
Timo Hegnauer <t.hegnauer@gmail.com>
2018-02-12 20:44:23 +0100

    simple bot with emulator

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
 /// <reference path="../node_modules/botbuilder/lib/botbuilder.d.ts" />
+/// <reference path="../node_modules/@types/restify/index.d.ts" />
+
+
 import * as builder from "botbuilder";
+import * as restify from "restify";
 
-let conn = new builder.ConsoleConnector().listen();
-// bot takes connector and default dialog
+let server = restify.createServer();
+server.listen(process.env.port|| process.env.PORT || 3978, () => {
+    console.log(`listening...${server.name}... ${server.url}`)
+});
+
+let conn = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
 let bot = new builder.UniversalBot(conn);
+//rout incoming posts to the handler conn.listen
+server.post("/api/messages", conn.listen());
 
+//Waterfall method, goes from function to function, tells it whats next
 bot.dialog("/", [
     (sess, args, next) => {
         if(!sess.userData.name){
@@ -17,5 +32,5 @@ bot.dialog("/", [
     (sess, result) =>{
          sess.userData.name = result.response;
          sess.send(`Hello, ${sess.userData.name}`);
-    }
-])
+	}
+]) 
