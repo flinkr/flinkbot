0c666595a4bb0d6055df2f75bb60f856339610a7
Timo Hegnauer <t.hegnauer@gmail.com>
2018-02-12 16:37:53 +0100

    example with waterfall dialog

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ flinkbot
 
 
 to run console bot, run node server.js
+sess.endDialog(); returns to root dialog
+
+put variables into strings in terminal: sess.send(`Hello, ${sess.userData.name}`);
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,19 @@ import * as builder from "botbuilder";
 
 let conn = new builder.ConsoleConnector().listen();
 // bot takes connector and default dialog
-let bot = new builder.UniversalBot(conn, (sess) =>{
-    sess.send("welcome to my bot!");
-})
+let bot = new builder.UniversalBot(conn);
+
+bot.dialog("/", [
+    (sess, args, next) => {
+        if(!sess.userData.name){
+            builder.Prompts.text(sess, "Hello, user! What is your name?");
+        }
+        else{
+            next();
+        }
+    },
+    (sess, result) =>{
+         sess.userData.name = result.response;
+         sess.send(`Hello, ${sess.userData.name}`);
+    }
+])
