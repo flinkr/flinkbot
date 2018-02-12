bdd08ddc7553f67a253ba8b7da92be14c07b6bc1
Timo Hegnauer <t.hegnauer@gmail.com>
2018-02-12 15:51:25 +0100

    hello world console bot

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 flinkbot
+
+
+to run console bot, run node server.js
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,8 @@
 /// <reference path="../node_modules/botbuilder/lib/botbuilder.d.ts" />
 import * as builder from "botbuilder";
 
+let conn = new builder.ConsoleConnector().listen();
+// bot takes connector and default dialog
+let bot = new builder.UniversalBot(conn, (sess) =>{
+    sess.send("welcome to my bot!");
+})
