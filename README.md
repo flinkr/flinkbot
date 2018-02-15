a10b87dca490367da8c99ac27d5d64ebcf2f4b19
Timo Hegnauer <t.hegnauer@gmail.com>
2018-02-15 11:00:03 +0100

    connected to luis with .env

--- README.md
+++ README.md
@@ -5,3 +5,28 @@ to run console bot, run node server.js
 sess.endDialog(); returns to root dialog
 
 put variables into strings in terminal: sess.send(`Hello, ${sess.userData.name}`);
+
+
+use replaceDialog to ask to change dialog
+use begindialog to ask for something and then return
+
+
+
+catch text that user wrote: result.response.entity === "yes"
+
+check which dialog the user came from directly (and not started over later)
+- in the first append a arg to the replacedialog sess.replaceDialog("/noResults", {entry: "dialog"})
+- in the secciond make if(args && args.entry && args.entry === "dialog")
+
+
+
+Get what exactly the user typed in instead of just intend (and save it as dialog data):
+-sess.dialogData.input = args.matched.input;
+
+
+Types:
+-sess: builder.Session
+
+
+
+For support of .env files, dotenv was used.
--- src/app.1.ts
+++ src/app.1.ts
@@ -2,6 +2,7 @@ import * as builder from "botbuilder";
 import * as dotenv from "dotenv";
 import * as restify from "restify";
 import * as flinkapi from "./flinkapi";
+
 dotenv.config();
 
 function logIntents(args: any): void {
@@ -20,20 +21,36 @@ const conn = new builder.ChatConnector({
 	appPassword: process.env.MICROSOFT_APP_PASSWORD,
 });
 
-const LuisModelUrl = process.env.LUIS_MODEL_URL;
+const LuisModelUrl = 'https://westeurope.api.cognitive.microsoft.com/luis/v2.0/apps/455bc78d-5193-448f-a2cc-bc02c55732df?subscription-key=de1144d124ba42719a63f71d49e499e3&verbose=true&timezoneOffset=0&q='
 const bot = new builder.UniversalBot(conn);
 bot.recognizer(new builder.LuisRecognizer(LuisModelUrl));
+// route incoming posts to the handler conn.listen
 server.post("/api/messages", conn.listen());
 
-bot.dialog("/FAQAddress",
-	(session, args) => {
-		logIntents(args);
-		session.send(`The address is Bahnhofstrasse 5!`)
-		session.endDialog();
-	}
-).triggerAction({
-	matches: "FAQ: Adresse von Flink",
+bot.dialog("/FAQAddress", (sess, args) => {
+	sess.send(`The address is Bahnhofstrasse 5!`);
+}).triggerAction({
+	matches: "/FAQ: Adresse von Flink",
 });
+
+// intents.onDefault([
+// (sess, args, next) => {
+// 		sess.userData.arrival = undefined;
+// 		sess.userData.departure = undefined;
+// 		if (!sess.userData.name) {
+// 			sess.beginDialog("/profile");
+// 		} else {
+// 			next();
+// 		}
+// 	},
+// 	(sess, result) => {
+// 		sess.send(`Hello ${sess.userData.name}, how can I help you?`)
+// 	},
+// ]);
+
+// //register intents to bot, trigger it first
+// var intents = new builder.IntentDialog({ recognizers: [recognizer] })
+// bot.dialog("/", intents);
 
 bot.dialog("/", [
 	(sess, args, next) => {
