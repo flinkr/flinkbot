f402d08752f324355b29a0f54820952b2a968ae8
Timo Hegnauer <t.hegnauer@gmail.com>
2018-02-19 14:32:27 +0100

    try

--- src/flinkapi.ts
+++ src/flinkapi.ts
@@ -1,41 +1,41 @@
-import * as rp from 'request-promise';
+import * as rp from "request-promise";
 import * as restify from "restify";
 
-
-
-
-export async function getZipCode(){
-    try{
-        let token = await login("blah", "blah");
-        let userInfo= await getUserInfo(token);
-        let zipCode = userInfo.zipCode;
-        return zipCode;
-    } catch (err){
-        return err;
-    }
+export async function getZipCode() {
+	try {
+		// tslint:disable-next-line:prefer-const
+		let token = await login("blah", "blah");
+		// tslint:disable-next-line:prefer-const
+		let userInfo = await getUserInfo(token);
+		// tslint:disable-next-line:prefer-const
+		let zipCode = userInfo.zipCode;
+		return zipCode;
+	} catch (err) {
+		return err;
+	}
 }
 
-export function login(email: string, password: string){
-    console.log("Login Triggered");
-    const options = {
-        method: 'POST',
-        uri: 'https://test.goflink.ch/api/v1/auth',
-        body: {
-            "username": "test@test.ch",
-            "password": "Plokiupl1"
-        },
-        json: true // Automatically stringifies the body to JSON
-    };
-    //returns a promise
-    return rp(options);
+export function login(email: string, password: string) {
+	console.log("Login Triggered");
+	const options = {
+		method: "POST",
+		uri: "https://test.goflink.ch/api/v1/auth",
+		body: {
+			"username": "test@test.ch",
+			"password": "Plokiupl1",
+		},
+		json: true, // Automatically stringifies the body to JSON
+	};
+	// returns a promise
+	return rp(options);
 }
 
-export function getUserInfo(token: string){
-    const options = {
-        method: 'GET',
-        uri: 'https://test.goflink.ch/api/v1/customers/me',
-        headers: { Authorization: 'Bearer '+token },
-        json: true // Automatically stringifies the body to JSON
-    };
-    return rp(options);
+export function getUserInfo(token: string) {
+	const options = {
+		method: "GET",
+		uri: "https://test.goflink.ch/api/v1/customers/me",
+		headers: { Authorization: "Bearer " + token },
+		json: true, // Automatically stringifies the body to JSON
+	};
+	return rp(options);
 }
