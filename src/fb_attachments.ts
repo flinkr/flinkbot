6ecc6ea3b9f94e381a17a7b2eb03c602c6d2106a
Timo Hegnauer <t.hegnauer@gmail.com>
2018-02-19 14:13:10 +0100

    mudularize webview

--- src/fb_attachments.ts
+++ src/fb_attachments.ts
@@ -1,38 +1,34 @@
-const facebookLoginWebview = [
-	{
-		"text": {
-			"text": ["testing now"],
-		},
-		"platform": "FACEBOOK",
-	},
-	{
-		"payload": {
-			"facebook": {
-				"attachment": {
-					"payload": {
-						"buttons": [
-							{
-								"url": "https://www.goflink.ch",
-								"title": "Logi",
-								"messenger_extensions": true,
-								type: "web_url",
-								webview_height_ratio: "compact",
-							},
-						],
-						"template_type": "button",
-						"text": "login",
-					},
-					"type": "template",
+// format according to channel's requirements
+// https://github.com/Microsoft/BotBuilder-Samples/blob/master/Node/blog-customChannelData/app.js
+// https://developers.facebook.com/docs/messenger-platform/reference/buttons/url
+
+export function facebookLoginWebview() {
+	return {
+		facebook: {
+			// format according to channel's requirements
+			// https://github.com/Microsoft/BotBuilder-Samples/blob/master/Node/blog-customChannelData/app.js
+			// https://developers.facebook.com/docs/messenger-platform/reference/buttons/url
+			attachment: {
+				type: "template",
+				payload: {
+					template_type: "generic",
+					elements: [
+						{
+							title: "Bitte bei Flink einloggen",
+							// subtitle: "This is subtitle",
+							buttons: [
+								{
+									type: "web_url",
+									url: "https://www.goflink.ch",
+									title: "Login",
+									webview_height_ratio: "compact",
+									"messenger_extensions": true,
+								},
+							],
+						},
+					],
 				},
 			},
 		},
-		platform: "FACEBOOK",
-	},
-	{
-		text: {
-		text: [
-			"testing now",
-		],
-	},
-}];
-export default facebookLoginWebview;
+	};
+}
