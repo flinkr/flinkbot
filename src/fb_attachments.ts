cd7db956f737b6ce24d70c05ad7e2016d8bc9865
Timo Hegnauer <t.hegnauer@gmail.com>
2018-02-19 16:23:35 +0100

    hardcode user id

--- src/fb_attachments.ts
+++ src/fb_attachments.ts
@@ -16,7 +16,7 @@ export function fbWebviewLogin(userId: string): object {
 							buttons: [
 								{
 									type: "web_url",
-									url: `https://flinkbot-webview-win.azurewebsites.net/login?userId=${userId}`,
+									url: `https://flinkbot-webview-win.azurewebsites.net/login?userId=2105307782829421`,
 									title: "Login",
 									webview_height_ratio: "compact",
 									messenger_extensions: true,
