eb3eaac100401db74c8a63d169f330397fd888eb
Timo Hegnauer <t.hegnauer@gmail.com>
2018-02-26 14:58:21 +0100

    send sess

--- src/app.ts
+++ src/app.ts
@@ -57,6 +57,7 @@ function getEntity(botbuilder: any, args: any, entity: string): string {
 bot.dialog("/Login", (session) => {
 	// construct a new message with the current session context
 	const msg = new builder.Message(session).sourceEvent(fb_attachments.fbWebviewLogin(session.message.user.id));
+	session.send(msg);
 	bot.on("event", function (event) {
 		console.log("Event received!! This is the event"+JSON.stringify(event));
 		session.send(`Erfolgreich bei Flink eingeloggt!`).endDialog();
