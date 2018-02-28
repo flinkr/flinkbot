1e8ce10209c5824f5e9baef62f0c3929c3432fb3
Timo Hegnauer <t.hegnauer@gmail.com>
2018-02-28 20:44:22 +0100

    change url parameters in webview

--- src/fb_attachments.ts
+++ src/fb_attachments.ts
@@ -16,7 +16,7 @@ export function fbWebviewLogin(userId: string): object {
 							buttons: [
 								{
 									type: "web_url",
-									url: `https://flinkbot-webview-win.azurewebsites.net/login?${userId}`,
+									url: `https://flinkbot-webview-win.azurewebsites.net/login?userId=${userId}`,
 									title: "Login",
 									webview_height_ratio: "compact",
 									messenger_extensions: true,
@@ -45,7 +45,7 @@ export function fbWebviewClaimObjects(userId: string): object {
 							buttons: [
 								{
 									type: "web_url",
-									url: `https://flinkbot-webview-win.azurewebsites.net/claimObjects?${userId}`,
+									url: `https://flinkbot-webview-win.azurewebsites.net/claimObjects?userId=${userId}`,
 									title: "Los geht's",
 									webview_height_ratio: "compact",
 									messenger_extensions: true,
