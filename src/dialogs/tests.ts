839386f6b17202db31eae3de050a1555d66046e5
Timo Hegnauer <t.hegnauer@gmail.com>
2018-03-21 16:17:09 +0100

    test

--- src/dialogs/tests.ts
+++ src/dialogs/tests.ts
@@ -19,6 +19,7 @@ export const createLibrary = () => {
 
 	lib.dialog("/Test Dialog", [
 		(session, result, next) => {
+			session.send("test dialog triggered");
 			// construct a new message with the current session context
 			const msg = new builder.Message(session).sourceEvent(fb_attachments.fbWebviewClaimObjects(process.env.WEBVIEW_URL, session.message.user.id, "claim1"));
 			session.send(msg);
